9461d9aff76a0fbc4e348cc01f7823f93150add6
21B6001 <21b6001@ubd.edu.bn>
2023-08-18 10:53:35 +0800

    added by 21B6001

---
+++ even_square_list.py
@@ -0,0 +1,13 @@
+# list = [1, 2, 3, 4, 5, 6]
+list = [22, 27,90]
+
+def even_square_list(list):
+    number = []
+    for num in list:
+        if num % 2 == 0:
+            number.append(num ** 2)
+    return number
+
+final = even_square_list(list)
+
+print(final)
